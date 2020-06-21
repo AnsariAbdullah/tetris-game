b89fce3aeddddcd53039026ebfc1fbc15e129a59
AnsariAbdullah <ansariabdullahar@gmail.com>
2020-06-21 17:22:30 +0530

    add colors

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ document.addEventListener('DOMContentLoaded', ()=>{
 	let nextRandom = 0;
 	let timerId;
 	let score = 0;
+	const colors = [
+		'orange',
+		'red',
+		'purple',
+		'green',
+		'blue'
+	]
 
 	// The Tetraminos
 	const lTetromino = [
@@ -59,14 +66,16 @@ document.addEventListener('DOMContentLoaded', ()=>{
 	// draw the Tetromino
 	function draw() {
     current.forEach(index => {
-      squares[currentPosition + index].classList.add('tetromino')
+			squares[currentPosition + index].classList.add('tetromino');
+			squares[currentPosition + index].style.backgroundColor = colors[random];
     })
   }
 
   //undraw the tetromino
   function undraw() {
     current.forEach(index => {
-      squares[currentPosition + index].classList.remove('tetromino')
+			squares[currentPosition + index].classList.remove('tetromino');
+			squares[currentPosition + index].style.backgroundColor = ''
     })
   }
 
@@ -171,9 +180,11 @@ document.addEventListener('DOMContentLoaded', ()=>{
 		//remove any trace of a tetromino form the entire grid
 		displaySquares.forEach(square => {
 			square.classList.remove('tetromino');
+			square.style.backgroundColor = '';
 		})
 		upNextTetrominoes[nextRandom].forEach( index => {
 			displaySquares[displayIndex + index].classList.add('tetromino');
+			displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom];
 		})
 	}
 
@@ -201,6 +212,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         row.forEach(index => {
 					squares[index].classList.remove('taken');
 					squares[index].classList.remove('tetromino');
+					squares[index].style.backgroundColor = '';
         })
         const squaresRemoved = squares.splice(i, width);
 				squares = squaresRemoved.concat(squares);
